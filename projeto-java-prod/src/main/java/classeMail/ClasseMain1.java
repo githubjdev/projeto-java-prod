5f2c0057dfabb6f542a2278c99c5bbb93b87a1ee
alex_ <alex.fernando.egidio@gmail.com>
2026-06-23 13:28:05 -0300

    dddd

---
+++ projeto-java-prod/src/main/java/classeMail/ClasseMain1.java
@@ -0,0 +1,11 @@
+package classeMail;
+
+public class ClasseMain1 {
+
+	public static void main(String[] args) {
+		System.out.println("Olá Java ddfd");
+		System.out.println("Olá Java GitHub - Cobrança ds");
+		System.out.println("Olá Java GitHub - Cobrança Correções");
+	}
+
+}
